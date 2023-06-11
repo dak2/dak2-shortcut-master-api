f4d2684dfc3e5aa5023330ced0f8d1dd9634b85e
dak2 <dawt2h@gmail.com>
2023-06-11 22:28:35 +0900

    enable google-auth-redirect

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { GoogleOAuthGuard } from './google-oauth.guard';
 
-@Controller()
+@Controller('auth')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(GoogleOAuthGuard)
+  googleAuth(@Request() req) {
+    void req;
+  }
+
+  @Get('google-redirect')
+  @UseGuards(GoogleOAuthGuard)
+  googleAuthRedirect(@Request() req) {
+    return this.appService.googleLogin(req);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  googleLogin(req) {
+    if (!req.user) {
+      return 'No user from google';
+    }
+    console.log(11111, req.user);
+
+    return {
+      message: 'User information from google',
+      user: req.user,
+    };
   }
 }
